d587b112c01658c25cba46fbabc367117abebd4a
rubengm <garciamendez.re@gmail.com>
2024-01-25 17:37:40 -0600

    Correccion de los archivos del laboratorio 4

--- Laboratorios/Laboratorio_4/course.py
+++ Laboratorios/Laboratorio_4/course.py
@@ -1,11 +1,13 @@
-class Course():
+# Definición de la clase Course
+class Course:
+    # Constructor de la clase Course
     def __init__(self, course_code, course_name):
         self.course_code = course_code
         self.course_name = course_name
 
+    # Método que muestra la información del curso
     def display_info(self):
-        print(f"Course code: {self.course_code} \
-              \nCourse name: {self.course_name}\n")
+        print(f"Course code: {self.course_code}\nCourse name: {self.course_name}\n")
         
     
 
--- Laboratorios/Laboratorio_4/university_management.py
+++ Laboratorios/Laboratorio_4/university_management.py
@@ -1,10 +1,11 @@
+# Se importan las clases Student, Teacher y Course 
 from student import Student
 from teacher import Teacher
 from course import Course
 
+# Creación de instancias de estudiantes, profesores y cursos
 student1 = Student("Esteban", 43, "B30754")
 student2 = Student("Maria", 28, "B50854")
-
 
 teacher1 = Teacher("Jorge Romero", 65, "12345")
 teacher2 = Teacher("Profesor X", 53, "72490")
@@ -12,16 +13,14 @@ teacher2 = Teacher("Profesor X", 53, "72490")
 course1 = Course("IE0217", "DSA")
 course2 = Course("IE0320", "CD1")
 
-
+# Inscripción a cursos y asignación de cursos
 student1.enroll_course(course1.course_code)
 student1.enroll_course(course2.course_code)
 
 teacher1.assign_course(course1.course_code)
 teacher2.assign_course(course2.course_code)
 
-
+# Mostrar información de estudiante, profesor y curso
 student1.display_info()
-
 teacher1.display_info()
-
 course1.display_info()
