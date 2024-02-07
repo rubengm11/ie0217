516e572d52f579f80d00a6fdd398c11807df1a34
rubengm <garciamendez.re@gmail.com>
2024-02-06 19:28:26 -0600

    Avance tarea 5, faltan graficos

--- Tareas/TAREA_CINCO/parte1/main.py
+++ Tareas/TAREA_CINCO/parte1/main.py
@@ -5,9 +5,10 @@ def cargar_datos(ruta_archivo):
         # Cargar el archivo CSV en un DataFrame
         df = pd.read_csv(ruta_archivo)
 
-        # Realizar operaciones de limpieza y preparacion de datos
+        # Limpieza de datos de forma iterativa, utilizando la columna DEST_CITY_NAME
         for indice, fila in df.iterrows():
-            df.at[indice, 'nombre'] = transformacion_personalizada(fila['nombre'])
+            df.at[indice, 'DEST_CITY_NAME'] = transformacion_personalizada(fila['DEST_CITY_NAME'])
+            
 
         return df
 
@@ -25,11 +26,65 @@ def transformacion_personalizada(valor):
     # Aqui va la logica de preparacion de datos
     return valor.upper()  
 
+def generar_informe_detallado(datos):
+    # Agrupar por la columna 'CARRIER_NAME'
+    grupos = datos.groupby('CARRIER_NAME')
+
+    for nombre_aerolinea, grupo in grupos:
+        cantidad_vuelos = len(grupo)
+        cantidad_pasajeros = grupo['PASSENGERS'].sum()
+
+
+        # Devolver información detallada como un diccionario
+        yield {
+            'Aerolínea': nombre_aerolinea,
+            'Cantidad de Vuelos': cantidad_vuelos,
+            'Cantidad de Pasajeros': cantidad_pasajeros,
+        }
+
+
 
 # Llamada a la función para cargar datos
-datos = cargar_datos("archivo.csv")
+datos = cargar_datos("T_T100D_MARKET_ALL_CARRIER.csv")
 
 # Verificar que los datos se hayan cargado bien
 if datos is not None:
     print("Datos cargados exitosamente:")
-    print(datos.head())
+    print(datos.head(15))
+
+    # Eliminando las columnas DEST (es redundante) y MAIL (es irrelevante)
+    datos = datos.drop('DEST', axis=1)
+    datos = datos.drop('MAIL', axis=1)
+
+    # Convirtiendo la columna de cantidad de pasajeros a int
+    datos['PASSENGERS'] = datos['PASSENGERS'].astype(int)
+
+    print(datos.head(15))
+
+    print("Info del dataframe")
+
+    # Proporciona información sobre el DataFrame
+    #print(datos.info())
+
+    # Proporciona estadísticas descriptivas del DataFrame
+    print(datos.describe())
+
+    # Cálculo del promedio de la columna 'PASSENGERS'
+    promedio_pasajeros = datos['PASSENGERS'].mean()
+    print("\nPromedio de pasajeros:", promedio_pasajeros, "\n")
+
+    # Cálculo de la moda de la columna 'DEST_CITY_NAME'
+    moda_ciudad_destino = datos['DEST_CITY_NAME'].mode()
+    print("\nCiudad destino mas frecuentada (moda)\n", moda_ciudad_destino, "\n")
+
+    # Maxima distancia de vuelo
+    distancia_maxima = datos['DISTANCE'].max()
+    print("\nDistancia maxima de vuelo:", distancia_maxima, "\n")
+
+    # Aerolinea mas usada
+    moda_aerolinea = datos['CARRIER_NAME'].mode()
+    print("\nAerolinea mas utilizada (moda)\n", moda_aerolinea, "\n")
+
+
+    for informe in generar_informe_detallado(datos):
+        print(informe, "\n")
